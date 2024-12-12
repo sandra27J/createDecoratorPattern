59964913367d721379f648e7856e3d8191987c44
A00306078 <A00306078@mycambrian.ca>
2024-12-11 23:05:26 -0500

    added color pattern

---
+++ ColorDecorator.cs
@@ -0,0 +1,14 @@
+namespace DecoratorPatternExample
+{
+    public class ColorDecorator : Decorator<string>
+    {
+        public ColorDecorator(IComponent<string> component) : base(component)
+        {
+        }
+
+        public override string GetText()
+        {
+            return $"\u001b[31m{base.GetText()}\u001b[0m";
+        }
+    }
+}
